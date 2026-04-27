d3eb26c8eab330f9e7e675958d3cda5348de6217
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-27 17:16:17 -0500

    feat(i18n): add internationalization support with vue-i18n.

--- src/main.js
+++ src/main.js
@@ -17,8 +17,10 @@ import {
     Textarea, Toast,
     ToastService, Toolbar, Tooltip
 } from "primevue";
+import i18n from "./i18n.js";
 
 createApp(App)
+    .use(i18n)
     .use(PrimeVue, { theme: { preset: Material}, ripple: true})
     .use(ConfirmationService)
     .use(DialogService)
