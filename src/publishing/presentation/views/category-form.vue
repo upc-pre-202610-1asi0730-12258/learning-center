fd8a42629630b72552ce65ed66bb3ae918c71377
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-03 05:48:26 -0500

    feat(category): add category management with creation, editing, and deletion functionality.

--- src/publishing/presentation/views/category-form.vue
+++ src/publishing/presentation/views/category-form.vue
@@ -41,7 +41,20 @@ const navigateBack = () => {
 </script>
 
 <template>
-
+  <div class="p-4">
+    <h1>{{ isEdit ? t('category.edit-title') : t('category.new-title') }}</h1>
+    <form @submit.prevent="saveCategory">
+      <div class="field mb-3">
+        <label for="name">{{ t('category.name') }}</label>
+        <pv-input-text id="name" v-model="form.name" class="w-full" required/>
+      </div>
+      <pv-button :label="t('category.save')" icon="pi pi-save" type="submit"/>
+      <pv-button :label="t('category.cancel')" class="ml-2" severity="secondary" @click="navigateBack"/>
+    </form>
+    <div v-if="errors.length" class="text-red-500 mt-3">
+      {{ t('errors.occurred') }}: {{ errors.map(e => e.message).join(', ') }}
+    </div>
+  </div>
 </template>
 
 <style scoped>
